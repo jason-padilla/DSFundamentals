d17b4d6720efb51c2302925194bdabf096811d30
Jason Padilla <jason-padilla@users.noreply.github.com>
2021-10-07 22:56:46 -0700

    leetcode 237

--- Leetcode/leetcode.py
+++ Leetcode/leetcode.py
@@ -1122,3 +1122,20 @@ class Solution:
       return self.lowestCommonAncestor(root.left, p, q)
     else:
       return root 
+
+* 237 DELETE NODE IN A LINKED LIST
+
+'''
+  Write a function to delete a node in a singly-linked list. 
+  You will not be given access to the head of the list, instead you will be given access to the node to be deleted directly.
+  It is guaranteed that the node to be deleted is not a tail node in the list.
+'''
+
+class Solution:
+  def deleteNode(self, node):
+    """
+    :type node: ListNode
+    :rtype: void Do not return anything, modify node in-place instead.
+    """
+    node.val = node.next.val 
+    node.next = node.next.next
