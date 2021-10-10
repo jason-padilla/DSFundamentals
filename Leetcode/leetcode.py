5f43f955204bed3a80765f382cce470bcdee54f6
Jason Padilla <jason-padilla@users.noreply.github.com>
2021-10-10 15:03:21 -0700

    leetcode 278

--- Leetcode/leetcode.py
+++ Leetcode/leetcode.py
@@ -1174,7 +1174,26 @@ class Solution:
     '''
         Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
     '''
-
     class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        return len(nums)*(len(nums)+1)//2 - sum(nums)
+        def missingNumber(self, nums: List[int]) -> int:
+            return len(nums)*(len(nums)+1)//2 - sum(nums)
+
+* 278 FIRST BAD VERSION 
+    '''
+        You are a product manager and currently leading a team to develop a new product. 
+        Unfortunately, the latest version of your product fails the quality check. 
+        Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+        Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+        You are given an API bool isBadVersion(version) which returns whether version is bad. 
+        Implement a function to find the first bad version. You should minimize the number of calls to the API.
+    '''
+    class Solution:
+        def firstBadVersion(self, n):
+            l, r = 0, n
+            while l < r:
+            mid = (l+r)//2      
+            if isBadVersion(mid):
+                r = mid
+            else:
+                l = mid + 1
+            return l
